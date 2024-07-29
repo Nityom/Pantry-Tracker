c96d9d791ebbfdc5ca3c2c34ff413eef26507d7b
Nityom Tikhe <nityomtikherr@gmail.com>
2024-07-29 22:04:19 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import React from 'react';
 import AddPantryItem from './components/AddPantryItem';
 import PantryList from './components/PantryList';
-import { CoolModeDemo } from './components/Button';
+import FlipTextDemo from './components/Heading';
+
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-          <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">Pantry Tracker</h1>
+          {/* <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">Pantry Tracker</h1> */}
+          <FlipTextDemo />
           
           <AddPantryItem />
           <PantryList />
---
+++ src/components/Heading.jsx
@@ -0,0 +1,12 @@
+import FlipText from './ui/SlightFlip'
+
+const FlipTextDemo = () => {
+  return (
+    <FlipText
+      className="text-3xl font-bold tracking-[-0.1em] text-black md:text-5xl md:leading-[5rem]"
+      word="Pantry Tracker"
+    />
+  );
+};
+
+export default FlipTextDemo;
